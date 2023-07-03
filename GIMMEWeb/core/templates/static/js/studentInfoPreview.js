1cf3a77b6e9f44b6d2bb81124bbacefa7e4836de
Linaiz <paulinajuzwin@gmail.com>
2023-07-03 17:33:44 +0100

    Display student name and email in preview.

--- GIMMEWeb/core/templates/static/js/studentInfoPreview.js
+++ GIMMEWeb/core/templates/static/js/studentInfoPreview.js
@@ -2,7 +2,10 @@ const previewElement = document.getElementById('student-info-preview');
 
 const placeholderElement = document.getElementById('student-info-placeholder');
 
+const studentName = document.getElementById('student-info-name');
+const studentEmail = document.getElementById('student-info-email');
 const personalityColumn = document.getElementById('student-info-personality');
+
 
 let serverState = undefined;
 
@@ -13,6 +16,12 @@ function previewStudentInfo(studentId){
         return;
 
     studentInfo = JSON.parse(serverState.studentsStates[studentId]);
+    console.log(studentInfo);
+
+
+    studentName.textContent = studentInfo.fullName;
+
+    studentEmail.textContent = studentInfo.email;
 
     personalityColumn.textContent = studentInfo.personality;
 
